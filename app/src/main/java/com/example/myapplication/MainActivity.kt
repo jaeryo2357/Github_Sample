732ce63b806c9746cbf0ddc3c8f4504789bab1e2
jaeryo2357 <33657541+jaeryo2357@users.noreply.github.com>
2023-08-29 20:55:32 +0900

    4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!123",
+        text = "Hello $name!1234",
         modifier = modifier
     )
 }
